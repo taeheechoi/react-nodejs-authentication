dcf9ab18ace204adb83104f51abb9e95380237b4
Tae Hee Choi <dadac76@hotmail.com>
2023-06-26 23:04:32 -0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+### Reference
+- https://medium.com/@Sukhjinder/react-and-nodejs-authentication-with-refresh-access-tokens-a-step-by-step-guide-343c7a722aec
---
+++ server/data/data.js
@@ -0,0 +1,20 @@
+const users = [
+    {
+        id: 1,
+        name: "John Doe",
+        email: "johndoe@example.com",
+        userName: "johndoe",
+        password: "JohnDoe@123"
+    },
+    {
+        id: 2,
+        name: "Jane Smith",
+        email: "janesmith@example.com",
+        userName: "janesmith",
+        password: "JaneSmith@123"
+    },
+]
+
+const tokens = [];  // [{userId: number, refreshToken: string, expirationTime: number }]
+
+module.exports = { users, tokens };
